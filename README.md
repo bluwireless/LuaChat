ad312ca6389aeb46b2833365d617ba9a76de0740
James Pascoe <james@james-pascoe.com>
2020-02-19 05:55:14 +0000

    LuaChat First Commit
    This repository contains a sample application to demonstrate
    how to combine C++17 and Lua 5.3 (using SWIG and CMake). LuaChat
    allows two users to connect (via TCP) and to exchange messages in
    a similar way to the Unix 'talk' program.
    
    Each commit documents a key step in the code's development. This
    commit provides enough code for the repository to be built and run,
    though with no functionality.

---
+++ README.md
@@ -0,0 +1,3 @@
+# LuaChat
+
+A Unix 'talk' application based on C++17 and Lua 5.3. [![Build Status](https://travis-ci.org/gabime/spdlog.svg?branch=master)](https://travis-ci.org/jamespascoe/LuaChat)
---
+++ src/lua_chat_main.cpp
@@ -0,0 +1,9 @@
+// Copyright(c) 2020, James Pascoe
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "Hello World" << std::endl;
+}
