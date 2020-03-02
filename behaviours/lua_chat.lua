85ad95793430e9d306fd7e1248bb3629ee3f81d8
James Pascoe <james.pascoe@bluwireless.com>
2020-03-02 10:44:15 +0000

    Implement LuaChat Behaviour
    This commit implements the 'full' LuaChat behaviour. This includes
    corutines for sending and receiving messages plus a dispatcher that
    runs the individual coroutines. There are several points of interest:
    
    i)  The dispatcher code is a simplified version of the dispatcher
        presented in section 9.4 of Programming in Lua:
    
        https://www.lua.org/pil/9.4.html
    
        Note that the addition of a blocking wait is to prevent LuaChat
        from always requesting the processor (and so consuming 100% of
        the CPU).
    
    ii) The use of rpoll in the sender coroutine is necessary to prevent
        the sender from blocking indefinitely (i.e. with io.read). If
        io.read is called regardless, then the sender coroutine will block
        until the user enters a string. Thus, messages sent by the peer will
        not be displayed until io.read returns.

--- behaviours/lua_chat.lua
+++ behaviours/lua_chat.lua
@@ -11,6 +11,93 @@
 
 ]]
 
+function sender (talk, host, port)
+
+  while true do
+
+    local ret = require 'posix'.rpoll(0, 1000)
+    if (ret == 1) then
+      local message = io.read()
+      if (message ~= "") then
+
+        local ret = talk:Send(
+          tostring(host), tostring(port), tostring(message)
+        )
+
+        if (ret == Actions.Talk.ErrorType_SUCCESS) then
+          Actions.Log.info(
+            string.format(
+              "Message sent to %s:%s %s", host, port, message
+            )
+          )
+        end
+      end
+    end
+
+    coroutine.yield()
+
+  end
+
+end
+
+function receiver (talk, host, port)
+
+  while true do
+
+    -- Yield until a message arrives, at which point, print it
+    repeat
+      coroutine.yield()
+    until talk:IsMessageAvailable()
+
+    message = talk:GetNextMessage()
+
+    Actions.Log.info(
+      string.format(
+        "Received from %s:%s %s", host, port, message
+      )
+    )
+
+    print(host .. ":" .. tostring(port) .. "> " .. message)
+
+  end
+
+end
+
+-- Coroutine dispatcher (see Section 9.4 of 'Programming in Lua')
+function dispatcher (coroutines)
+
+  local timer = Actions.Timer()
+
+  while true do
+    if next(coroutines) == nil then break end -- no more coroutines to run
+
+    for name, co in pairs(coroutines) do
+      local status, res = coroutine.resume(co)
+
+      if res then -- coroutine has returned a result (i.e. finished)
+
+        if type(res) == "string" then  -- runtime error
+          Actions.Log.critical("Lua coroutine '" .. tostring(name) ..
+                               "' has exited with runtime error " .. res)
+        else
+          Actions.Log.warn("Lua coroutine '" .. tostring(name) .. "' exited")
+        end
+
+        coroutines[name] = nil
+
+        break
+      end
+    end
+
+    -- Run the dispatcher every 1 ms. Note, that this is required to prevent
+    -- LuaChat from consuming 100% of a core. Note also that the notification
+    -- value is set to the maximum value of a uint32_t to prevent conflicts
+    -- with user timer ids (i.e. user timers can start from 0 and count up).
+    timer(Actions.Timer.WaitType_BLOCK, 1, "ms", 0xffffffff)
+
+  end
+end
+
 local function main(args)
 
   print("Welcome to Lua Chat !")
@@ -38,6 +125,30 @@ local function main(args)
                         "same host e.g. -a port=7777 -a server_port=8888")
     end
   end
+
+  -- Create a 'talk' object
+  local talk
+  if (args["server_port"]) then
+    talk = Actions.Talk(args["server_port"])
+  else
+    talk = Actions.Talk()
+  end
+
+  -- Set the destination port to the default (if necessary)
+  if (not args["port"]) then
+    args["port"] = Actions.Talk.default_port
+  end
+
+  -- Create co-routines
+  local coroutines = {}
+  coroutines["receiver"] = coroutine.create(receiver)
+  coroutine.resume(coroutines["receiver"], talk, args["host"], args["port"])
+
+  coroutines["sender"] = coroutine.create(sender)
+  coroutine.resume(coroutines["sender"], talk, args["host"], args["port"])
+
+  -- Run the main loop
+  dispatcher(coroutines)
 
 end
 
